d1d95441f61c8c2fe88e1cf56e380425e4ab2def
mikhalev <mikhalev@rp.team>
2018-04-05 08:36:45 +0300

    empty project

---
+++ TheActors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheActors
+//
+//  Created by Михалев Александр on 05/04/2018.
+//  Copyright © 2018 RP Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
